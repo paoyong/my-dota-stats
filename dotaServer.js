ddb5ca0142f0ed5bf36185ff0eeae6e3985faf5e
Keith Yong <terda12@gmail.com>
2015-02-06 09:07:39 -0500

    Placeholder basic code

--- dotaServer.js
+++ dotaServer.js
@@ -1,55 +1,24 @@
-var http = require('http');
-var fs = require('fs');
-var jsdom = require('jsdom');
+var fs = require('fs'),
+    jsdom = require('jsdom'),
+    express = require('express'),
+    path = require('path'),
 
-/* Dota modules */
-var matchDetails = require('./getMatchJSONFromId');
-var dotadb = require('./dotadb');
+    /* Custom modules */
+    matchDetails = require('./getMatchJSONFromId'),
+    dotadb = require('./dotadb'), 
 
-var port = 8888;
-
-/* Digital Ocean server port */
-var ip = '104.131.118.167';
+    /* Digital Ocean server port and IP */
+    port = 8888,
+    ip = '104.131.118.167',
+    main_router = require("./routes/index");
 
 /* Comment this line when deploying to Git and the server */
 //var ip = 'localhost';
 
-/* Start the server */
-var server = http.createServer(function (req, res) {
-    console.log("Recieved request: " + req.url);
-    if (req.url === '/') {
-        call_jsdom("index.html", function(window) {
-            var $ = window.$;
-            dotadb.getLatestDotaMatch(function(match) {
-                console.log("Found latest match! Match: " + match);
-                var hours = matchDetails.getHoursSinceGameWasPlayed(match.match_time, match.match_duration).toFixed(1);
-                console.log("Found the latest match: " + match.match_id);
-                
-                /* Write to the HTML file with JQuery */
-                res.writeHead(202, {'Content-Type': 'text/html'});
-                $("#hours_ago").text(hours);
-                res.end("<!DOCTYPE html>\n" + $('html').html());
-            });
-        });
-    } else {
-        /* End the request if it's something other than '/' */
-        res.end();
-    }
-}).listen(port, ip);
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use('/', main_router);
 
-function call_jsdom(source, callback) {
-    jsdom.env(
-            source,
-            ['jquery-2.1.3.js'],
-            function(errors, window) {
-                process.nextTick(
-                function() {
-                    if (errors) {
-                        throw new Error("There were errors: " + errors);
-                    }
-                    callback(window);
-                }
-            );
-        }
-    );
-}
+// Start the server.
+app.listen(port);
