38f6179eea12f78222e1acaacacdd7a54e21c29e
Keith Yong <terda12@gmail.com>
2015-02-14 18:48:15 -0500

    qtip2 tooltips added, some style changes, some attribute changes to SVG rects

--- public/js/dataWriter.js
+++ public/js/dataWriter.js
@@ -1,7 +1,7 @@
 function drawData(dotaJSON) {
     var width = 0,
         height = 0,
-        cellSize = 20,
+        cellSize = 25,
         cellMarginRight = 3,
         maxOpacityTippingPoint = 6,    // For example, if this value is 10, 10 would be represented with the strongest color in the chart.
         minOpacity = 0.0,
@@ -21,7 +21,8 @@ function drawData(dotaJSON) {
     // Draw SVG canvas on #dota-histogram
     var svg = dotaHistogram.append('svg')
         .attr('width', width)
-        .attr('height', height);
+        .attr('height', height)
+        .attr('id', 'dota-histogram-canvas');
 
     // For each data point, draw a square
     svg.selectAll('rect').data(dotaJSONKeyArray).enter()
@@ -31,13 +32,58 @@ function drawData(dotaJSON) {
         .attr('x', function(d) {
             return d * (cellSize + cellMarginRight);
         })
-        .attr('fill', 'rgb(256, 0, 0')
+        .attr('fill', 'rgb(256, 30, 20)')
         .attr('fill-opacity', function(d) {
             return opacityScale(dotaJSON[d]);
         })
-        .attr('stroke', 'black')
-        .attr('stroke-opacity', 0.5);
+        .attr('days-ago', function(d) {
+            return d;
+        })
+        .attr('matches-played', function(d) {
+            return dotaJSON[d];
+        });
 }
+
+function drawTooltips() {
+    $('rect').each(function() {
+        var daysAgo = parseInt($(this).attr('days-ago'), 10),
+            matchesPlayed = parseInt($(this).attr('matches-played'), 10),
+            tooltipText = '';
+
+        if (matchesPlayed === 0){
+            tooltipText = 'No matches played ';
+        } else {
+            tooltipText = matchesPlayed + ' matches played ';
+        }
+
+        if (daysAgo === 0) {
+            tooltipText += 'today!';
+        }
+        else if (daysAgo === 1) {
+            tooltipText += 'yesterday!';
+        } else {
+            tooltipText += daysAgo + ' days ago!';
+        }
+
+        $(this).qtip({
+            content: {
+                text: tooltipText
+            },
+            position: {
+                my: 'bottom center',
+                at: 'top center', 
+                target: $(this)
+            },
+            style: {
+                classes: 'qtip-tipsy'
+            },
+            show: {
+                delay: 20
+            }
+        });
+    });
+}
+
 // Grab data
 $.get('/hours-played', function(response) {
     $('#hours-ago').text(response);
@@ -46,4 +92,6 @@ $.get('/hours-played', function(response) {
 // Grab match counts and draw it out
 $.get('/dota-match-counts', function(response) {
     drawData(response);
+    drawTooltips();
 });
+
