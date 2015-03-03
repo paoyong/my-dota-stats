7d113ea02bbfbd33153df5945cc666dee48e351b
Keith Yong <terda12@gmail.com>
2015-03-03 09:12:24 -0500

    Added configjs

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.steam_api_key = '29270A858787255DA9648F82763CBB35';
+config.dota_account_id = '94636820';
+config.dota_match_counts_json_filename = 'dotaMatchCounts.json';
+config.days = 30; 
+config.refresh_minutes = 5; 
+
+module.exports = config;
